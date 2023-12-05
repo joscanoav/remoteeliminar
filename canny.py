7654e5ec2c3dc8b4ac5f1f2d7180e3f9ad66c362
joscanoav <joscanoav@gmail.com>
2023-12-05 11:13:29 +0100

    Comentarios en el python

--- canny.py
+++ canny.py
@@ -1,11 +1,12 @@
+#Bibliotecas
 import cv2
-
+#Abrir Imagenes
 img = cv2.imread('libro.jpg', 0)
 borderCanny = cv2.Canny(img, 100, 200)
-
+#Mostrar imagenes
 cv2.imshow('original', img)
 cv2.imshow('blur', borderCanny)
-
+#Presionar una tecla para dejar de ejecutar
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
